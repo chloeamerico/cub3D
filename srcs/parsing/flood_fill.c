f7f686e96e33dd59d39b7abc74035df1cb78e9b8
Chloe Americo <camerico@f2r11s12.clusters.42paris.fr>
2025-10-23 14:44:04 +0200

    good pour le parsing de la map

--- srcs/parsing/flood_fill.c
+++ srcs/parsing/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 12:56:08 by camerico          #+#    #+#             */
-/*   Updated: 2025/10/22 18:04:15 by camerico         ###   ########.fr       */
+/*   Updated: 2025/10/23 14:42:16 by camerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ static char	**copy_map(t_game *game)
 }
 
 
+
 // //fonction pour explorer la map
 static int	flood_fill(char **map, int x, int y, t_game *game)
 {
@@ -62,16 +63,11 @@ static int	flood_fill(char **map, int x, int y, t_game *game)
 	ft_printf("on check le char %c en ligne %i et colonne %i\n", map[y][x], y, x);
 	if (map[y][x] == '1' || map[y][x] == 'V')
 		return (0);
-	if(map[y][x] == ' ' || map[y][x] == '\n' || map[y][x] == '\0')		//si c'est un espace, on check si il est sur un bord
-	{
-		if(y == 0 || y == game->data->map_height - 1
-			|| x == 0 || x == (int)ft_strlen(map[y]) - 1)
-			return (1);
-		return (0);
-	}
+	if(map[y][x] == '\n' || map[y][x] == '\0')		//si c'est un espace, on check si il est sur un bord
+		return (1);
 	if (map[y][x] != '0' && map[y][x] != 'N' && map[y][x] != 'S' 
         && map[y][x] != 'E' && map[y][x] != 'W')
-        return (0);
+        return (1);
 	map[y][x] = 'V';
 	if(flood_fill(map, x - 1, y, game) == 1)
 		return (1);
@@ -84,24 +80,57 @@ static int	flood_fill(char **map, int x, int y, t_game *game)
 	return (0);
 }
 
-//une fois qu'on a valide que la map etait valide, on remplace les espaces par des 0 sur la map de base
-static void	replace_space(t_game *game)
-{
-	int i;
-	int j;
-	i = 0;
-	while (i < game->data->map_height)
-	{
-		j = 0;
-		while (game->map[i][j])
-		{
-			if (game->map[i][j] == ' ')
-		    	game->map[i][j] = '0';
-			j++;
-		}
-		i++;
-	}
-}
+
+// // //fonction pour explorer la map
+// static int	flood_fill(char **map, int x, int y, t_game *game)
+// {
+// 	 if (y < 0 || y >= game->data->map_height)		//on check si on est dans les lim de la map
+//         return (1);
+// 	 if (x < 0 || x >= (int)ft_strlen(map[y]))		//on check si on est dans les lim de cette ligne
+//         return (1);
+// 	ft_printf("on check le char %c en ligne %i et colonne %i\n", map[y][x], y, x);
+// 	if (map[y][x] == '1' || map[y][x] == 'V')
+// 		return (0);
+// 	if(map[y][x] == ' ' || map[y][x] == '\n' || map[y][x] == '\0')		//si c'est un espace, on check si il est sur un bord
+// 	{
+// 		if(y == 0 || y == game->data->map_height - 1
+// 			|| x == 0 || x == (int)ft_strlen(map[y]) - 1)
+// 			return (1);
+// 		return (0);
+// 	}
+// 	if (map[y][x] != '0' && map[y][x] != 'N' && map[y][x] != 'S' 
+//         && map[y][x] != 'E' && map[y][x] != 'W')
+//         return (0);
+// 	map[y][x] = 'V';
+// 	if(flood_fill(map, x - 1, y, game) == 1)
+// 		return (1);
+// 	if (flood_fill(map, x + 1, y, game) == 1)
+// 		return (1);
+// 	if(flood_fill(map, x, y - 1, game) == 1)
+// 		return(1);
+// 	if (flood_fill(map, x, y + 1, game) == 1)
+// 		return (1);
+// 	return (0);
+// }
+
+// //une fois qu'on a valide que la map etait valide, on remplace les espaces par des 0 sur la map de base
+// static void	replace_space(t_game *game)
+// {
+// 	int i;
+// 	int j;
+// 	i = 0;
+// 	while (i < game->data->map_height)
+// 	{
+// 		j = 0;
+// 		while (game->map[i][j])
+// 		{
+// 			if (game->map[i][j] == ' ')
+// 		    	game->map[i][j] = '0';
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
 
 //la fonction qui verifie si le chemin est valide
 int	valid_path(t_game *game)
@@ -121,6 +150,12 @@ int	valid_path(t_game *game)
 		ft_printf("Error : Map not closed\n");
 		return (1);
 	}
-	replace_space(game);
+	// replace_space(game);
+	// i = 0;								//pour afficher la map
+	// while(game->map[i])
+	// {
+	// 	ft_printf("%s", game->map[i]);
+	// 	i++;
+	// }
 	return (0);
 }
